c54478615b8ae51d37e43165c140cd941ab9a15c
shravya <shravyau111@gmail.com>
2024-07-03 09:11:49 +0530

    slider

--- src/Slider.js
+++ src/Slider.js
@@ -5,44 +5,37 @@ function Slider() {
     {
       image: "https://picsum.photos/200/300",
       title: "This is a title",
-      description: "This is a description",
-      clickEvent: sliderClick,
+      description: "This is a description"
     },
     {
       image: "https://picsum.photos/600/500",
       title: "This is a second title",
-      description: "This is a second description",
-      clickEvent: sliderClick,
+      description: "This is a second description"
     },
     {
       image: "https://picsum.photos/700/600",
       title: "This is a third title",
-      description: "This is a third description",
-      clickEvent: sliderClick,
+      description: "This is a third description"
     },
     {
       image: "https://picsum.photos/500/400",
       title: "This is a fourth title",
-      description: "This is a fourth description",
-      clickEvent: sliderClick,
+      description: "This is a fourth description"
     },
     {
       image: "https://picsum.photos/200/300",
       title: "This is a fifth title",
-      description: "This is a fifth description",
-      clickEvent: sliderClick,
+      description: "This is a fifth description"
     },
     {
       image: "https://picsum.photos/800/700",
       title: "This is a sixth title",
-      description: "This is a sixth description",
-      clickEvent: sliderClick,
+      description: "This is a sixth description"
     },
     {
       image: "https://picsum.photos/300/400",
       title: "This is a seventh title",
-      description: "This is a seventh description",
-      clickEvent: sliderClick,
+      description: "This is a seventh description"
     },
   ];
 
